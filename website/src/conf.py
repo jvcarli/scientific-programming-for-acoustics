1d92e17b2a152ea53746f3351c48723dd08bccda
Joao Vitor Carli Pereira <jvitor.carli@gmail.com>
2023-10-28 14:55:01 -0300

    feat!: change sphinx html to piccolo_theme

--- website/src/conf.py
+++ website/src/conf.py
@@ -31,5 +31,5 @@ gettext_compact = False
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
+html_theme = 'piccolo_theme'
 html_static_path = ['_static']
